a8e383173a3911a65ce370556c207fb939b7d4f4
rafmillan <rafael.k.millan@gmail.com>
2019-10-27 14:30:56 -0700

    fixed attributes printing out of order

--- scrapper.py
+++ scrapper.py
@@ -6,26 +6,27 @@ print('Trae Young: ')
 response = requests.get('https://www.espn.com/nba/player/_/id/4277905/trae-young')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-headers = {'GP', 'MIN', 'FG%', '3P%', 'FT%', 'REB',	'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS'}
+headers = ['GP', 'MIN', 'FG%', '3P%', 'FT%', 'REB',	'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS']
 
 stats = soup.find_all(class_='Table__TR Table__TR--sm Table__even')[4]
 statsList = []
 
 
-i = 0;
+i = 0
 for stat in stats:
 	stat = stats.find_all(class_='Table__TD')[i].get_text()
 	statsList.append(stat)
 	i+=1
+
+j = 0
+for name in headers:
+	print(name, ': ', statsList[j])
+	j+=1
 
 with open('stats.csv', 'w') as csv_file:
     csv_writer = writer(csv_file)
     csv_writer.writerow(headers)
     csv_writer.writerow(statsList)
 
-print(headers)
-print(statsList)
+#print(headers)
 #print(statsList)
-
-
-
